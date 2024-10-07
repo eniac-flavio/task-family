eac6fb0e3cf4a705e32d86103fbb34761ecbb408
kayky1111 <106442023@eniac.edu.br>
2024-10-07 19:33:41 -0300

    ✨ Feat: Foi adicionado uma interação ao botão do sou responsavel, que redireciona o usuario a pagina de login

--- app/src/main/java/com/example/task_family/GreetingActivity.java
+++ app/src/main/java/com/example/task_family/GreetingActivity.java
@@ -1,6 +1,9 @@
 package com.example.task_family;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,24 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class GreetingActivity extends AppCompatActivity {
 
+    private Button adultButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_greeting);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Encontra o botão pelo ID
+        adultButton = findViewById(R.id.adult);
+
+        // Configura o listener de clique para o botão
+        adultButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Cria um Intent para abrir a LoginActivity
+                Intent intent = new Intent(GreetingActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
