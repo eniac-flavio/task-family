de627e1e89b4bd389dbddf60a9869c1cae2bd7b8
kayky1111 <106442023@eniac.edu.br>
2024-10-07 16:08:47 -0300

    :sparkles: Feat: Foi adicionada uma nova Activity para o Login, dando outra funcionalidade ao MainActivity, adicionando também ao AndroidManifest.xml o LoginActivity

--- app/src/main/java/com/example/task_family/MainActivity.java
+++ app/src/main/java/com/example/task_family/MainActivity.java
@@ -1,65 +1,24 @@
 package com.example.task_family;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText txtEmail;
-    private Button validatorButton;
-    private EmailValidatorManager emailValidatorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-        inicializarUI();
-        initializeViews();
-        setupEmailValidatorManager();
-        setupListeners();
-    }
-
-    private void inicializarUI() {
-        configurarBotaoRegistrar();
-    }
-
-    private void configurarBotaoRegistrar() {
-        findViewById(R.id.registerButton).setOnClickListener(v -> abrirTelaRegistro());
-    }
-
-    private void abrirTelaRegistro() {
-        startActivity(new Intent(this, RegistrarActivity.class));
-    }
-
-    private void initializeViews() {
-        txtEmail = findViewById(R.id.txtEmail);
-        validatorButton = findViewById(R.id.validatorButton);
-    }
-
-    private void setupEmailValidatorManager() {
-        emailValidatorManager = new EmailValidatorManager(this, txtEmail);
-    }
-
-    private void setupListeners() {
-        txtEmail.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    emailValidatorManager.validateEmail();
-                    return true;
-                }
-                return false;
-            }
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
         });
-
-        validatorButton.setOnClickListener(v -> emailValidatorManager.validateEmail());
     }
 }
-
