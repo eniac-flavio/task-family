992dbdd09e9b7d92332d3d0841cbebff79df875c
Kevin <106012023@eniac.edu.br>
2024-10-07 16:34:13 -0300

    :sparkles: Feat: Atualização visual, de modelos e adição da atividade do galo

--- app/src/main/java/com/example/task_family/LoginActivity.java
+++ app/src/main/java/com/example/task_family/LoginActivity.java
@@ -18,7 +18,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.activity_login);
 
         inicializarUI();
         initializeViews();
--- app/src/main/java/com/example/task_family/RegistrarActivity.java
+++ app/src/main/java/com/example/task_family/RegistrarActivity.java
@@ -19,7 +19,7 @@ public class RegistrarActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.registrar);
+        setContentView(R.layout.activity_registrar);
 
         // Inicializar as views
         initializeViews();
